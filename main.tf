6432d475a3cbc2111829150e3c86916a1ecb66b2
niftom <ade@seciti.com>
2024-11-26 10:06:58 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,6 +2,6 @@ provider "aws" {
   region = "us-west-1"
 }
 resource "aws_instance" "WebServer" {
-  ami           = "ami-012967cc5a8c9f891"
+  ami           = "ami-0c7217cdde317cfec"
   instance_type = "t2.micro"
 }
